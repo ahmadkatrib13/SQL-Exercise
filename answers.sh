506ff2a56f46ed9f050d1dcc45b805dea13e945f
ahmadkatrib13 <ahmadkatrib123@gmail.com>
2021-12-13 11:44:42 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,17 @@
+SELECT * FROM students;
+#basic queires
+#1
+SELECT name FROM students;
+#2
+SELECT * FROM students WHERE Age>30;
+#3
+SELECT name FROM students WHERE Age = 30 AND Gender like 'F';
+#5
+ INSERT INTO students VALUES('7','ahmad',21,'M',500);
+ #6
+ UPDATE students SET Points = Points+10 WHERE name="Basma";
+#7
+ UPDATE students SET Points = Points-10 WHERE name="Alex";
+#8 updated
+#9 done
+
